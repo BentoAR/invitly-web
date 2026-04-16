c280b0581ebc1daf974a2ff612f6b64dceafe509
Lautaro clerice <clericelautaro@gmail.com>
2026-04-16 19:58:18 -0300

    feat: Update FAQ component to use getLocale for locale retrieval and streamline imports

--- components/features/home/FAQ.tsx
+++ components/features/home/FAQ.tsx
@@ -1,14 +1,13 @@
 export const dynamic = "force-static";
 
-import { getTranslations } from "next-intl/server";
-import { useLocale } from "next-intl";
+import { getTranslations, getLocale } from "next-intl/server";
 import FAQClient from "./FAQClient";
 import StructuredData from "@/components/shared/StructuredData";
 import { getFAQSchema } from "@/src/utils/structuredData";
 
 export default async function FAQ() {
   const t = await getTranslations("FAQ");
-  const locale = useLocale();
+  const locale = await getLocale();
   const faqs = t.raw("faqs") as Array<{
     question: string;
     answer: string;
